b92cf21109b269f23e78c26a778fade4b9009232
Wiktor Wilk <mcserw4@wp.pl>
2023-04-10 20:32:54 +0200

    new path

--- src/main/java/net/exotia/plugins/economy/EconomyPlugin.java
+++ src/main/java/net/exotia/plugins/economy/EconomyPlugin.java
@@ -1,9 +1,8 @@
-package net.exotia.exotiaeconomy;
+package net.exotia.plugins.economy;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ExotiaEconomy extends JavaPlugin {
-
+public final class EconomyPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
